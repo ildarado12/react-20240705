615cfa51fb8bd2a80679f51fac129f20dae75a4f
Ildar Gataullin <ildarado12@yandex.ru>
2024-07-28 16:25:54 +0300

    выполнены задачи 5го ДЗ

--- src/components/counter/counter.jsx
+++ src/components/counter/counter.jsx
@@ -1,9 +1,17 @@
+import classnames from "classnames";
 import { Button } from "../button/button";
+import { useUser } from "../login-context/context";
 import styles from "./styles.module.css";
 
 export const Counter = ({ value = 0, increment, decrement }) => {
+  const { value: user } = useUser();
+
   return (
-    <div>
+    <div
+      className={classnames({
+        [styles.login]: user === `Login`,
+      })}
+    >
       <Button text={`-`} onClick={decrement} />
       <span className={styles.counter}>{value}</span>
       <Button text={`+`} onClick={increment} />
