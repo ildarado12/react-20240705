bc6409cd2ffebe86b3b0468f1b3061b52bc4670f
Ildar Gataullin <ildarado12@yandex.ru>
2024-09-03 23:57:01 +0300

    ДЗ 9: загрузка ресторанов с сервера

---
+++ src/redux/ui/request/request.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const requestSlice = createSlice({
+    name: `request`,
+    initialState: {},
+    extraReducers: (builder) => builder
+    .addMatcher(({type}) => type.endsWith(`pending`), (state, {meta}) => {
+        state[meta.requestId] = `pending`;
+    })
+    .addMatcher(({type}) => type.endsWith(`rejected`), (state, {meta}) => {
+        state[meta.requestId] = `rejected`;
+    })
+    .addMatcher(({type}) => type.endsWith(`fulfilled`), (state, {meta}) => {
+        state[meta.requestId] = `fulfilled`;
+    }),
+    selectors: {selectRequestStatus: (state, requestId) => state[requestId] || `idle`},
+});
+
+export const {selectRequestStatus} = requestSlice;
